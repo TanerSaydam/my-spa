3288ed4ea7490a539036cdae93ff5329f9973481
tanersaydam <tanersaydam@gmail.com>
2023-09-21 17:48:22 +0300

    .

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -37,12 +40,15 @@ class Router {
                     template = yield fetchTemplate(new component().templateUrl);
                 }
                 appRoot.innerHTML = template;
+                new component();
             }
         });
     }
     static route(path) {
         if (path == "home")
             return HomeComponent;
+        else if (path == "data-binding")
+            return DataBindingComponent;
         return AppComponent;
     }
 }
@@ -50,7 +56,11 @@ let AppComponent = class AppComponent {
 };
 AppComponent = __decorate([
     Component({
-        template: `<h1>App Component</h1><button route="home">Go to Home</button>`
+        template: `
+        <h1>App Component</h1>
+        <button route="home">Go to Home</button>
+        <button route="data-binding">Go to Data Binding</button>
+        `
     })
 ], AppComponent);
 let HomeComponent = class HomeComponent {
@@ -60,6 +70,39 @@ HomeComponent = __decorate([
         templateUrl: './components/home.component.html'
     })
 ], HomeComponent);
+let DataBindingComponent = class DataBindingComponent {
+    constructor() {
+        this.data = "";
+        this.setupBindings();
+    }
+    setupBindings() {
+        const inputElement = document.getElementById("dataInput");
+        if (inputElement) {
+            inputElement.addEventListener("input", (e) => {
+                this.data = e.target.value;
+            });
+        }
+        const logButton = document.getElementById("logButton");
+        if (logButton) {
+            logButton.addEventListener("click", () => {
+                this.logData();
+            });
+        }
+    }
+    logData() {
+        console.log(this.data);
+    }
+};
+DataBindingComponent = __decorate([
+    Component({
+        template: `
+    <h1>Two-way Binding Example</h1>
+    <input id="dataInput" type="text"/>
+    <button id="logButton">Log Data</button>
+  `
+    }),
+    __metadata("design:paramtypes", [])
+], DataBindingComponent);
 window.addEventListener("load", () => __awaiter(void 0, void 0, void 0, function* () {
     yield Router.navigate(AppComponent);
     const routeElements = document.querySelectorAll("[route]");
--- src/main.ts
+++ src/main.ts
@@ -25,18 +25,23 @@ class Router {
               template = await fetchTemplate(new component().templateUrl);
             }
             appRoot.innerHTML = template;
+            new component();
         }
     }
 
     static route(path: string):any{
         if(path == "home") return HomeComponent
-
+        else if(path == "data-binding") return DataBindingComponent
         return AppComponent;
     }
 }
 
 @Component({
-    template: `<h1>App Component</h1><button route="home">Go to Home</button>`
+    template: `
+        <h1>App Component</h1>
+        <button route="home">Go to Home</button>
+        <button route="data-binding">Go to Data Binding</button>
+        `
 })
 class AppComponent {}
   
@@ -45,6 +50,41 @@ class AppComponent {}
   })
   class HomeComponent {}
   
+  @Component({
+    template: `
+    <h1>Two-way Binding Example</h1>
+    <input id="dataInput" type="text"/>
+    <button id="logButton">Log Data</button>
+  `
+})
+
+class DataBindingComponent{
+ public data: string = "";
+
+ constructor(){
+    this.setupBindings();
+ }
+
+ setupBindings(){
+    const inputElement = document.getElementById("dataInput") as HTMLInputElement
+    if(inputElement){
+        inputElement.addEventListener("input",(e)=> {
+            this.data = (e.target as HTMLInputElement).value;
+        });
+    }
+
+    const logButton = document.getElementById("logButton") as HTMLButtonElement;
+    if(logButton){
+        logButton.addEventListener("click", ()=> {
+            this.logData();
+        });
+    }
+ }
+
+ logData(){
+    console.log(this.data);
+ }
+}
 
   window.addEventListener("load", async () => {
     await Router.navigate(AppComponent);
@@ -59,5 +99,5 @@ class AppComponent {}
         }
       });
     });
-});
-  
+}); 
+
