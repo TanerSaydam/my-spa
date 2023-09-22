37f5154f6fcb64d2f1db429fe2f85e382083e9c6
tanersaydam <tanersaydam@gmail.com>
2023-09-22 06:11:52 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -5,50 +5,71 @@ import { TestComponent } from "./components/test/test.component";
 
 
 async function fetchTemplate(url: string): Promise<string> {
-    const response = await fetch(url);
-    const text = await response.text();
-    return text;
-}  
+  const response = await fetch(url);
+  const text = await response.text();
+  return text;
+}
 
 class Router {
-    static async navigate(component: any, path: string){
-        const appRoot = document.getElementById("app");
-        if (appRoot) {
-            let template = new component().template;
-            if (new component().templateUrl) {
-              template = await fetchTemplate(new component().templateUrl);
-            }
-            appRoot.innerHTML = template;
-            new component();
-
-            history.pushState({}, '', '/' + path)
-        }
+  static async navigate(component: any, path: string) {
+    const appRoot = document.getElementById("app");
+    if (appRoot) {
+      let template = new component().template;
+      if (new component().templateUrl) {
+        template = await fetchTemplate(new component().templateUrl);
+      }
+      appRoot.innerHTML = template;
+      new component();
+      
+      setEventListeners();
+      history.pushState({ path }, '', '/' + path);
     }
+  }
 
-    static route(path: string):any{
-        if(path == "home") return HomeComponent
-        else if(path == "data-binding") return DataBindingComponent
-        else if(path == "test") return TestComponent
-        return AppComponent;
+  static route(path: string): any {
+    if (path == "home") return HomeComponent
+    else if (path == "data-binding") return DataBindingComponent
+    else if (path == "test") return TestComponent
+    return AppComponent;
+  }
+}
+
+const setEventListeners = () => {
+  const clickHandler = async (e: Event) => {
+    console.log(e);
+    const route = (e.currentTarget as HTMLElement).getAttribute("route");
+    if (route) {
+      const component = Router.route(route);
+      await Router.navigate(component, route);
     }
-}  
-  
+  };
 
-  window.addEventListener("load", async () => {
-    const initialPath = window.location.pathname.replace('/', '');
-    const initialComponent = Router.route(initialPath);
+  const routeElements = document.querySelectorAll("[route]");
+  routeElements.forEach((el) => {
+    el.removeEventListener("click", clickHandler); // Önceki event listener'ı kaldır
+    el.addEventListener("click", clickHandler); // Yeni event listener eklenir
+  });
+};
 
-    await Router.navigate(initialComponent,initialPath);
+window.addEventListener("load", async () => {
+  const initialPath = window.location.pathname.replace('/', '');
+  const initialComponent = Router.route(initialPath);
+  await Router.navigate(initialComponent, initialPath);  
+
+  setEventListeners();
   
-    const routeElements = document.querySelectorAll("[route]");
-    routeElements.forEach((el) => {
-      el.addEventListener("click", async (e) => {
-        const route = (e.currentTarget as HTMLElement).getAttribute("route");
-        if (route) {
-          const component = Router.route(route);
-          await Router.navigate(component, route);
-        }
-      });
-    });
-}); 
+  window.onpopstate = function (event) {
+    if (event.state && event.state.path !== undefined) {
+      const component = Router.route(event.state.path.replace('/', ''));     
+      Router.navigate(component, event.state.path);
+      setEventListeners();
+    }
+  };
+});
+
+
+// Geri veya ileri düğmesi tıklandığında
+
+
+
 
